2a9ded8cc707fd9fbac3145bbd42f77978cd0feb
govindmishra932 <govindmishra932@gmail.com>
2023-06-11 03:49:36 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyWorld</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/govindmishra__01" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/govindmishra001" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
